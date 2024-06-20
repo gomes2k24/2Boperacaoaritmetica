7d73b42fdc97675b5f65edb1ec4fc65a176c3625
gomes2k24 <160244732+gomes2k24@users.noreply.github.com>
2024-06-20 08:13:32 -0300

    cargo inicial

---
+++ 2Boperacaoaritmetica/script.js
@@ -0,0 +1,42 @@
+// +     Adição
+// -      Subtração
+// *     Multiplicação
+// /     Divisão
+// %    Módulo
+// ++  Incremento
+// --    decremento
+
+// var a = 78;
+// var b = 15;
+// var total= a + bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a - bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a * bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a / bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a % bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a ++ bc;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total= a -- bc;
+// console.log(total);
